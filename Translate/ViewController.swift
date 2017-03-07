e65567b25a7477205fc748cf982609cf62b6a5cc
20063209@mail.wit.ie <Erica@ericas-mac.local>
2017-03-07 12:39:33 +0000

    Language Selection and Minor Text Fixes

--- Translate/ViewController.swift
+++ Translate/ViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
     
     @IBOutlet weak var textToTranslate: UITextView!
     @IBOutlet weak var translatedText: UITextView!
-    
-    var languages: [String] = ["Japanese", "German", "Swedish"]
+    @IBOutlet weak var selectLang: UIPickerView!
+    @IBOutlet weak var langLabel: UILabel!
+    var languages: [String] = ["French", "Irish", "Japanese", "Korean"]
+    var langSelected = "fr"
     
     
     //var data = NSMutableData()
@@ -27,8 +29,13 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         view.addGestureRecognizer(tap)
         
+        //Code for picker
+        self.selectLang.dataSource = self;
+        self.selectLang.delegate = self;
+        
     }
     
+    //Function called to remove keyboard
     func dismissKeyboard()
     {
         view.endEditing(true)
@@ -39,12 +46,49 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return languages.count;
+    }
+    
+    //Adds the data into the picker
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return languages[row];
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        langLabel.text = languages[row]
+        
+        if(row == 0)
+        {
+            langSelected = "fr"
+        }
+        else if(row == 1)
+        {
+            langSelected = "ga"
+        }
+        else if(row == 2)
+        {
+            langSelected = "ja"
+        }
+            
+        else if(row == 3)
+        {
+            langSelected = "ko"
+        }
+        
+    }
+    
+    //Translates Function
     @IBAction func translate(_ sender: AnyObject) {
         
         let str = textToTranslate.text
         let escapedStr = str?.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
         
-        let langStr = ("en|ja").addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
+        let langStr = ("en|"+langSelected).addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
         
         let urlStr:String = ("https://api.mymemory.translated.net/get?q="+escapedStr!+"&langpair="+langStr!)
         
